48a4b3b525e0b459cc8c76478d678bad5e6d4c60
Dron2019 <mr.andrey@bigmir.net>
2021-06-03 15:57:00 +0300

    add CV inputs structure

--- src/assets/scripts/admin-panel/components/CreateCV/CreateCV.jsx
+++ src/assets/scripts/admin-panel/components/CreateCV/CreateCV.jsx
@@ -1,9 +1,15 @@
+/* eslint-disable max-len */
 import React, {useState} from 'react';
 
 import EmptyCV from '../EmptyCV/EmptyCV.jsx';
 
 function getFieldsForCV() {
   return {
+    groupNames: {
+      workExpirience: 'Досвід роботи',
+      education: 'Освіта',
+      workAbilities: 'Професійні навички',
+    },
     defaultFields: [
       { name: 'cvName', title: 'Назва шаблону:', value: '' },
       { name: 'name', title: 'Ім\'я:', value: '' },
@@ -14,11 +20,12 @@ function getFieldsForCV() {
       { name: 'self-info', title: 'Коротка інформація про себе (розкажіть хто ви)', value: '' },
     ],
     workAbilities: [
-      'Професійні навички',
-      { name: 'workAbilities_0', title: 'Наприклад знання Adobe Photoshop:', value: '' },
+
+      [
+        { name: 'workAbilities_0', title: 'Наприклад знання Adobe Photoshop:', value: '' },
+      ],
     ],
     workExpirience: [
-      'Місце роботи',
       [
         { name: 'work-company', title: 'Назва компанії:', value: '' },
         { name: 'work-position', title: 'Посада: (наприклад швачка, кухарка)', value: '' },
@@ -28,7 +35,13 @@ function getFieldsForCV() {
       ],
     ],
     education: [
-      'Освіта',
+      [
+        { name: 'education-name', title: 'Назва закладу:', value: '' },
+        { name: 'education-specialization', title: 'Спеціальність:', value: '' },
+        { name: 'education-description', title: 'Опис повний: (розкажіть, які навички ви отримали у процесі навчання)', value: '' },
+        { name: 'education-start', title: 'Дата вступу:', value: '' },
+        { name: 'education-end', title: 'Дата закінчення:', value: '' },
+      ],
       [
         { name: 'education-name', title: 'Назва закладу:', value: '' },
         { name: 'education-specialization', title: 'Спеціальність:', value: '' },
@@ -42,11 +55,11 @@ function getFieldsForCV() {
 
 
 export default function CreateCV() {
-  let [defaultFields, setDefaultFields] = useState(getFieldsForCV().defaultFields);
-  let [ workAbilities, setWorkAbilities] = useState(getFieldsForCV().workAbilities[1]);
-  let [ workExpirience, setworkExpirience] = useState(getFieldsForCV().workExpirience[1]);
-  let [ education, setEducation] = useState(getFieldsForCV().education[1]);
-  console.log(defaultFields);
+  let [ defaultFields, setDefaultFields] = useState(getFieldsForCV().defaultFields);
+  let [ workAbilities, setWorkAbilities] = useState(getFieldsForCV().workAbilities);
+  let [ workExpirience, setworkExpirience] = useState(getFieldsForCV().workExpirience);
+  let [ education, setEducation] = useState(getFieldsForCV().education);
+
   return (
     <div className="create-cv-wrapper">
       <div className="page-title text-violet">
@@ -57,6 +70,9 @@ export default function CreateCV() {
         <div className="form-std__subtitle text-violet">
           Створити нове резюме:
         </div>
+        <div className="button-std button-std--violet small">Додати фото</div>
+        <span className="text-gray"> (розмір фото 150х150)</span>
+        <input type="file" name="" id="" />
         {defaultFields.map(field => (
             <div className="input-group">
               <input type="text" className="input-std" value={field.value} placeholder={field.title} />
@@ -64,15 +80,29 @@ export default function CreateCV() {
         ))
         }
         <div className="input-section">
-          <div className="input-section__title"></div>
+          <div className="input-section__title text-violet">{getFieldsForCV().groupNames.workAbilities}</div>
+          {workAbilities.map(group => group.map((field, index) => <InputGroupCV field={field} index={index}/>))}
         </div>
-        {workAbilities.map(field => (
-            <div className="input-group">
-              <input type="text" className="input-std" value={field.value} placeholder={field.title} />
-            </div>
-        ))}
+        <div className="input-section">
+          <div className="input-section__title text-violet">{getFieldsForCV().groupNames.workExpirience}</div>
+          {workExpirience.map(group => group.map((field, index) => <InputGroupCV field={field} index={index}/>))}
+        </div>
+        <div className="input-section">
+          <div className="input-section__title text-violet">{getFieldsForCV().groupNames.education}</div>
+            {education.map(group => group.map((field, index) => <InputGroupCV field={field} index={index}/>))}
+        </div>
+        <button type="submit" className="button-std button-std--violet small">Створити резюме</button>
       </div>
     </div>
   );
 }
 
+function InputGroupCV(props) {
+  const { field } = props;
+  const { index } = props;
+  return (
+    <div className="input-group">
+      <input type="text" key={index} className="input-std" value={field.value} placeholder={field.title} />
+    </div>
+  );
+}
