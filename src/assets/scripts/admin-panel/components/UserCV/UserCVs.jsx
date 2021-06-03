7273c99ebb6d5a177d839821f4e980542b2e7e26
Dron2019 <mr.andrey@bigmir.net>
2021-06-03 10:34:52 +0300

    add CVs state array

--- src/assets/scripts/admin-panel/components/UserCV/UserCVs.jsx
+++ src/assets/scripts/admin-panel/components/UserCV/UserCVs.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 
 import routes from '../../routes/routes.jsx';
@@ -6,12 +6,12 @@ import { Logo } from '../icons/Icons.jsx';
 
 /* Изменить ссылку, когда будет создан компонент "Создать резюме" */
 export default function UserCV(props) {
-  console.log(routes);
+  const [cvs, setCV] = useState(getCVs());
+
   return (
     <div className="user-cv-wrapper">
         <div className="page-title text-violet">Мої резюме</div>
-        <EmptyCV/>
-        <ListCV/>
+        {cvs.length === 0 ? <EmptyCV/> : <ListCV items={cvs}/>}
     </div>
 
   );
@@ -19,7 +19,7 @@ export default function UserCV(props) {
 
 function EmptyCV(props) {
   return (
-        <div className="white-bg-element">
+        <div className="white-bg-element empty-cv-block">
             <div className="subtitle-small text-violet">Мої резюме:</div>
             <p>
                 У вас ще не створено жодногу резюме. Щоб створити резюме,
@@ -34,22 +34,34 @@ function EmptyCV(props) {
 
 function ListCV(props) {
   return (
-        <div className="white-bg-element">
+        <div className="white-bg-element cv-list">
             <div className="subtitle-small text-violet">Мої резюме:</div>
-            <SingleCV/>
+            {props.items.map((item) => <SingleCV title={item.title}/>)}
         </div>
   );
 }
 
 function SingleCV(props) {
+  const title = props.title || 'Без назви';
   return (
         <div className="curriculum-vitae">
           <div className="curriculum-vitae__icon">
             <Logo/>
           </div>
-          <div className="curriculum-vitae__title text-violet">Шаблон “Вчительки молодших класів”</div>
+          <div className="curriculum-vitae__title text-violet">{title}</div>
           <div className="curriculum-vitae__button curriculum-vitae__edit-button  max-content button-std button-std--violet small">Редагувати</div>
           <div className="curriculum-vitae__button curriculum-vitae__pdf-button max-content  button-std button-std--violet small">PDF</div>
         </div>
   );
+}
+
+function getCVs() {
+  return [
+    // { title: 'Шаблон “Вчительки молодших класів”', id: 0 },
+    // { title: 'Манікюрщиця', id: 0 },
+    // { title: 'Кухарка', id: 0 },
+    // { title: 'Офіціант', id: 0 },
+    // { title: 'Модель', id: 0 },
+    // { title: 'SMM-менеджер', id: 0 },
+  ]
 }
